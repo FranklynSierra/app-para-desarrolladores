e63c9bb87e5f3741845dcb05fa63324d21f52f2e
fran <franklynssierracontreras@gmail.com>
2022-07-27 08:54:19 -0400

    inicio de la union con axios

--- src/App.js
+++ src/App.js
@@ -14,34 +14,33 @@ import About from './pages/cruds/about';
 import { format } from 'date-fns';
 import { useState, useEffect } from 'react';
 import Footer from './pages/cruds/footer';
+import api from './api/posts';
 function App() {
 
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "My First Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 4,
-      title: "My Fourth Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+  const [posts, setPosts] = useState([]);
+  const [postBody,setPostBody]=useState('')
+  const [postTitle,setPostTitle]=useState('')
+  const [search,setSearch]=useState('')
+  const [searchResults, setSearchResults] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch (err) {
+        if (err.response) {
+          // Not in the 200 response range 
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
     }
-  ]);
+
+    fetchPosts();
+  }, [])
   const handleDelete=(id)=>{
     const postList=posts.filter(post=>post.id!==id)
     setPosts(postList)
@@ -58,10 +57,7 @@ function App() {
     setPostBody('');
     
   }
-  const [postBody,setPostBody]=useState('')
-  const [postTitle,setPostTitle]=useState('')
-  const [search,setSearch]=useState('')
-  const [searchResults, setSearchResults] = useState([]);
+ 
   useEffect(()=>{
     const filteredResults=posts.filter(post=>
       ((post.body).toLowerCase()).includes(search.toLowerCase())
